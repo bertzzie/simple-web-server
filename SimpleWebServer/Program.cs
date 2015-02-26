ead34f2fe4b498aa889c6e2ba72ac2020775a7b2
Alex Xandra Albert Sim <bertzzie@gmail.com>
2015-02-26 16:07:24 +0700

    Initial commit. Command options OK

---
+++ SimpleWebServer/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+using CommandLine;
+using CommandLine.Text;
+
+namespace SimpleWebServer
+{
+    # region Program Options
+    class ProgramOptions
+    {
+        [Option('p', "port", Required = false,
+            HelpText = "Nomor port untuk server",
+            DefaultValue = 8080)]
+        public int port { get; set; }
+
+        [HelpOption]
+        public string GetUsage()
+        {
+            var help = new HelpText
+            {
+                Heading = new HeadingInfo("Simple Web Server", "1.0"),
+                Copyright = new CopyrightInfo("Alex Xandra Albert Sim", 2015),
+                AdditionalNewLineAfterOption = true,
+                AddDashesToOption = true
+            };
+
+            help.AddPreOptionsLine("Lisensi: bebas digunakan, tetapi ingat program hanya untuk demo.");
+            help.AddPreOptionsLine("Penulis kode tidak bertanggung jawab atas kerusakan akibat penggunaan program.");
+            help.AddOptions(this);
+
+            return help;
+        }
+    }
+    #endregion Program Options
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int port;
+
+            var options = new ProgramOptions();
+            if (CommandLine.Parser.Default.ParseArguments(args, options))
+            {
+                port = options.port;
+                Console.WriteLine("The Port Used is {0}", port);
+            }
+        }
+    }
+}
